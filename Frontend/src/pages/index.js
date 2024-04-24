b5ec29f8eaf5a57569baffb78f7ca998537fd827
Aniket-Patel-swg <patelaniket1207@gmail.com>
2024-04-24 11:31:18 +0530

    frontend docker file and simple form

---
+++ Frontend/src/pages/index.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    company: '',
+    projectDescription: '',
+    budget: '',
+    deadline: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here, e.g., sending data to server
+    console.log(formData);
+    // Clear form fields after submission
+    setFormData({
+      name: '',
+      email: '',
+      company: '',
+      projectDescription: '',
+      budget: '',
+      deadline: ''
+    });
+  };
+
+  return (
+    <>
+       <div>
+      <h1>I'll make your dream website live</h1>
+      <p>Visit: <a href="https://aniketpatel.me/">aniketpatel.me</a></p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+        </label>
+        <br />
+        <label>
+          About Company:
+          <input type="text" name="company" value={formData.company} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Project Description:
+          <textarea name="projectDescription" value={formData.projectDescription} onChange={handleChange} required />
+        </label>
+        <br />
+        <label>
+          Budget:
+          <input type="text" name="budget" value={formData.budget} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Deadline:
+          <input type="date" name="deadline" value={formData.deadline} onChange={handleChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    </>
+  );
+}
